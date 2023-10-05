1988cc0eddbd1bd3c495738a027366556b345373
pgk95 <kalupgk@gmail.com>
2023-10-05 21:56:15 +0300

    quick fix

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """type-annotated function"""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """sum_list: type-annotated function"""
     return float(sum(input_list))
