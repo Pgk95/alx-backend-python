ed38debc9294671b52e1bb7461b51f286aacdabd
pgk95 <kalupgk@gmail.com>
2023-10-05 22:03:19 +0300

    quick fix on task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """type-annotated function"""
+from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """return values with the appropriate types"""
+    """takes a list lst of strings and returns a list of tuples
+    each tuple has a string and an int"""
     return [(i, len(i)) for i in lst]
