ef12bf02203a0328a36b37ee1497c5d7b8982e75
pgk95 <kalupgk@gmail.com>
2023-10-05 19:54:23 +0300

    quick fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
-
+"""type-annoted function"""
 def add(a: float, b: float) -> float:
+    """returns a and b as arguments and returns
+        their sum as a float
+    """
     return (a + b)
