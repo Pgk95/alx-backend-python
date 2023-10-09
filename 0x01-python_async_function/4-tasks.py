554567f34e68f0c6e1123ab8b3ed75be5bcf032b
pgk95 <kalupgk@gmail.com>
2023-10-09 15:04:53 +0300

    task 3 and 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""imported wait_random from the previous python file"""
+
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """returns list of all delays in ascending order"""
+    tasks = [wait_random(max_delay) for i in range(n)]
+    completed_tasks = await asyncio.gather(*tasks)
+
+    sorted_delays = sorted(completed_tasks)
+
+    return sorted_delays
