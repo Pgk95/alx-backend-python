7deecf82f84ac1b45dd206af8363307dc2a8018b
pgk95 <kalupgk@gmail.com>
2023-10-09 14:10:41 +0300

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+"""imported wait_random from the previous python file"""
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int = 0, max_delay: int = 10) -> float:
+    """"returns the list of all the delays (float values)."""
+    listed_delays = []
+    for i in range(n):
+        listed_delays.append(await wait_random(max_delay))
+        return sorted(listed_delays)
